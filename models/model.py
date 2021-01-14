0e2b0311c895a2e1f2100b905d3bc9ca818905b0
Gustavo Magalhães <gmagalhaess@indracompany.com>
2021-01-13 22:07:22 -0300

    Adds comments in resource.py and model.py

--- models/model.py
+++ models/model.py
@@ -1,28 +1,58 @@
+'''
+Model Module
+
+This module contains common helper functions and methods in all application. It's supposed to 
+include here any new shared function or methods.
+'''
+
 from __future__ import annotations
 from database.db import db, IntegrityError, SQLAlchemyError
 import datetime
 
 def string_to_date(string_date: str) -> datetime:
+    '''
+    Converts the string_date string (YYYY-MM-DD) to datetime type.
+    '''
     Y, m, d = [int(item) for item in string_date.split('-')]
 
     return datetime.datetime(Y, m, d)
 
 class DataBaseException(SQLAlchemyError):
+    '''
+    DataBaseException class
+
+    This class inherits from SQLAlchemyError class to be used as a Database Exception.
+    '''
     def __init__(self, message: str) -> None:
         self.message = message
 
 
 class Model():
+    '''
+    Model Class
+
+    This class contains common helper methods in all application. It's supposed to include here 
+    any new shared methods.
+    '''
     @classmethod
     def get_all(cls) -> list:
+        '''
+        Returns a list of items there are saved in database.
+        '''
         return cls.query.order_by(cls.id.desc()).all()
 
     @classmethod
     def find_by_id(cls, _id: int) -> Model:
+        '''
+        Returns the item found by id there is saved in database.
+        '''
         return cls.query.filter_by(id = _id).first()
     
     @classmethod
     def get_all_by_segment(cls, segment_id: int, order_by_date: bool = False) -> list:
+        '''
+        Returns a list of items by segment_id there are saved in database.
+        '''
         if not order_by_date:
             rows = cls.query.filter_by(segment_id = segment_id).order_by(cls.id.desc()).all()
         elif hasattr(cls, 'started_at'):
@@ -34,9 +64,15 @@ class Model():
     
     @classmethod
     def get_current_by_segment(cls, segment_id: int) -> Model:
+        '''
+        Returns the item by segment_id there is saved in database.
+        '''
         return cls.query.filter_by(segment_id = segment_id).order_by(cls.id.desc()).first()
 
     def save(self) -> None:
+        '''
+        Saves the item in database.
+        '''
         try:
             db.session.add(self)
             db.session.commit()
@@ -48,6 +84,9 @@ class Model():
             raise DataBaseException('An error occurred saving the item.')
 
     def delete(self) -> None:
+        '''
+        Deletes the item from database.
+        '''
         try:
             db.session.delete(self)
             db.session.commit()
--- resources/resource.py
+++ resources/resource.py
@@ -1,14 +1,27 @@
+'''
+Resource Module
+
+This module contains common helper functions and methods in all application. It's supposed to 
+include here any new shared function or methods.
+'''
+
 from models.model import Model, DataBaseException, string_to_date
 import datetime
 from typing import Optional
 
-def non_empty_string(string: str) -> Exception:
-    if not string:
+def non_empty_string(field: str) -> Exception:
+    '''
+    Checks if the field passed is empty.
+    '''
+    if not field:
         raise ValueError('Field must not be empty')
 
-    return string
+    return field
 
 def verify_date_field(field, data: str) -> str:
+    '''
+    Checks if the field passed is a datetime field and converts the content from string to datetime.
+    '''
     if field in ['started_at', 'ended_at', 'created_at', 'performed_at', 'issued_at', 'expires_at']:
         content = string_to_date(data)
     else:
@@ -17,34 +30,62 @@ def verify_date_field(field, data: str) -> str:
     return content
 
 def check_json(item: Model) -> Optional[Model]:
-        if item:
-            return item.json()
-        else:
-            return None
+    '''
+    Checks if the item passed is not None to return Model.json() properly.
+    '''
+    if item:
+        return item.json()
+    else:
+        return None
     
 def check_curriculum_json(item: Model) -> Optional[Model]:
+    '''
+    Checks if the item passed is not None to return Model.curriculum_json() properly.
+    '''
     if item:
         return item.curriculum_json()
     else:
         return None
 
 def list_map(collection: list) -> list:
+    '''
+    Returns a list of Model.json() from a collection.
+    '''
     return list(map(lambda item: check_json(item), collection))
 
 def list_map_curriculum(collection: list) -> list:
+    '''
+    Returns a list of Model.curriculum_json() from a collection.
+    '''
     return list(map(lambda item: check_curriculum_json(item), collection))
 
 class Resource():
+    '''
+    Resource Class
 
+    This class contains common helper methods in all application. It's supposed to include here 
+    any new shared methods.
+    '''
     def __init__(self, model: Model) -> None:
+        '''
+        Resource Constructor
+
+        Loads the model passed as param.
+        '''
         self.model = model
     
     def get_all(self) -> dict:
+        '''
+        Accesses Model.get_all() and returns a list of items.
+        '''
         items = self.model.get_all()
 
         return {'items': list_map(items)}, 200
     
     def find_by_id(self, _id: int) -> dict:
+        '''
+        Accesses Model.find_by_id() and retuns the item serached by id.
+        '''
         item = self.model.find_by_id(_id)
         if item:
             return {'item': check_json(item)}, 200
@@ -52,6 +93,9 @@ class Resource():
             return {'error': {'message': 'Item not found'}}, 400
     
     def store(self, data: dict) -> dict:
+        '''
+        Accesses Model.save() to insert the item load by passed data and returns a list of items saved.
+        '''
         item = self.model(**data)
         try:            
             item.save()
@@ -62,6 +106,9 @@ class Resource():
             return {'error': {'message': e.message}}, 500
     
     def update(self, _id, data: dict) -> dict:
+        '''
+        Accesses Model.save() to update the item found by passed id and returns a list of items saved.
+        '''
         item = self.model.find_by_id(_id)
         if item:
             try:
@@ -77,6 +124,9 @@ class Resource():
             return {'error': {'message': 'Item not found'}}, 400
     
     def destroy(self, _id: int) -> dict:
+        '''
+        Accesses Model.delete() to delete the item found by passed id and returns a list of items saved.
+        '''
         item = self.model.find_by_id(_id)
         if item:
             try:
